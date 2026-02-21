2cbb1136698aa48b66e7ac41390573bcaabf873b
vikboss1226 <vikboss1226@gmail.com>
2026-02-21 15:29:50 +0530

    added github actions

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Welcome to React App Test Muraly New CICD Pipeline</h1>
+      <h1>Welcome to React App Test Muraly New CICD Pipeline Test</h1>
     </div>
   );
 }
