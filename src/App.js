c4381a5ce4ccb83802b07e634e996f337b04a243
vikboss1226 <vikboss1226@gmail.com>
2026-02-21 11:51:57 +0530

    added github actions

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Welcome to React App Test</h1>
+      <h1>Welcome to React App Test 2</h1>
     </div>
   );
 }
