54f6d1121ce100585ad3cdc6138f28d7c1a3f96e
vikboss1226 <vikboss1226@gmail.com>
2026-02-21 12:52:49 +0530

    added github actions

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Welcome to React App Test Muraly</h1>
+      <h1>Welcome to React App Test Muraly 2</h1>
     </div>
   );
 }
